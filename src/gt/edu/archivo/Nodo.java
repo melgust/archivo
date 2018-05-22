d3678f1a5b4cd1c18f271bdf290b3e0ef010331a
Melvin Cali <melgust@gmail.com>
2018-05-21 19:20:49 -0600

    Se agrega clases para ejemplo de Pilas

---
+++ src/gt/edu/archivo/Nodo.java
@@ -0,0 +1,13 @@
+package gt.edu.archivo;
+
+public class Nodo {
+	
+	int elemento;
+	Nodo siguiente;
+
+	Nodo(int x) {
+		elemento = x;
+		siguiente = null;
+	}
+	
+}
---
+++ src/gt/edu/archivo/Pila.java
@@ -0,0 +1,46 @@
+package gt.edu.archivo;
+
+//codigo fuente copiado de https://pastebin.com/7JXij3uk
+
+public class Pila {
+
+	private Nodo cima;
+	public Pila() {
+		cima = null;
+	}
+
+	// operaciones
+	public boolean pilaVacia() {
+		if (cima == null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void insertar(int elemento) {
+		Nodo nuevo;
+		nuevo = new Nodo(elemento);
+		nuevo.siguiente = cima;
+		cima = nuevo;
+	}
+
+	public int quitar() {
+		if (pilaVacia()) {
+			return -1;
+		}
+		int aux = cima.elemento;
+		cima = cima.siguiente;
+		return aux;
+	}
+
+	public void limpiarPila() {
+		Nodo t;
+		while (!pilaVacia()) {
+			t = cima;
+			cima = cima.siguiente;
+			t.siguiente = null;
+		}
+	}
+
+}
